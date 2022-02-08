88e54f602e19fb5c115d0991f226176972903a95
Daniel Mrkoci <daniel@wiklunds.nu>
2022-02-08 13:59:44 +0100

    lektion 1

--- Lektion-1/1-start/start.js
+++ Lektion-1/1-start/start.js
@@ -1,7 +1,13 @@
 const hello = () => {
   console.log('Hello World');
 }
-hello()
-hello()
-hello()
-hello()
+// hello()
+// hello()
+// hello()
+// hello()
+
+console.log(global);
+
+setTimeout(() => {
+  console.log('timed out');
+}, 3000)
