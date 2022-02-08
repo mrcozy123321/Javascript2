4ff03cec862652607207ac02d296ca0dc24287d0
Daniel Mrkoci <daniel@wiklunds.nu>
2022-02-08 14:33:53 +0100

    lektion 1

---
+++ Lektion-1/2-modules/index.js
@@ -0,0 +1,6 @@
+// Egna moduler
+
+// import hej from './user'
+const hej = require('./user')
+
+console.log(hej);
---
+++ Lektion-1/2-modules/user.js
@@ -0,0 +1,8 @@
+const user = {
+  firstName: 'Daniel',
+  lastName: 'Mrkoci',
+  age: 29,
+  email: 'daniel@mail.com'
+}
+
+console.log(user);
