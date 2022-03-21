691ee44819e25b46dc0d8b68c4e06c8b655b87e6
Daniel Mrkoci <daniel@wiklunds.nu>
2022-03-21 18:07:19 +0100

    lektion 4

---
+++ Lektion-4/1-websocket-chat/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const socket = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.static(path.join(__dirname, 'public')));
