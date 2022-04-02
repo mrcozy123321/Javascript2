8b926d0bcd6f61325a3c28108db033323804073b
Daniel Mrkoci <daniel@wiklunds.nu>
2022-04-02 20:58:10 +0200

    lektion 6

---
+++ Lektion-6/2-intro2/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <Navbar/>
+  <Carousel/>
+  <Testimonials/>
+  <Footer/>
+</template>
+
+<script>
+import Navbar from './components/navigation/Navbar.vue'
+import Footer from './components/navigation/Footer.vue'
+import Carousel from './components/Carousel.vue'
+import Testimonials from './components/Testimonials/Testimonial.vue'
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    Footer,
+    Carousel,
+    Testimonials
+  }
+}
+</script>
+
+<style>
+
+</style>
